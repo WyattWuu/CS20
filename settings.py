b9add1e3d5a30ea63935189f181ff152af8016da
Wyatt <49630338+WyattWuu@users.noreply.github.com>
2024-04-09 22:43:34 +1000

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ if os.name == 'nt':
     os.environ['PATH'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
     os.environ['PROJ_LIB'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
     # 阿芙蓉vFDAVS大概吧sdfhksdfhjks
+    # dfghjk
